d9dc2c728b3719ab65bb182cc8e9856726323e88
konnono <konnono@gmail.com>
2021-06-22 22:08:58 +0800

    [Feat] Render all records on index page

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+
+const Record = require('./models/record')
+const Category = require('./models/category')
+
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => {
+      res.render('index', { records })
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
