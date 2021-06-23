64af8013a7fa480e2c89c26c6bcb791456005095
konnono <konnono@gmail.com>
2021-06-23 21:36:45 +0800

    [Feat] Add edit single record runction

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('./node_modules/body-parser')
-
+const methodOverride = require('method-override')
+const Handlebars = require('handlebars')
 require('./config/mongoose')
 
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+const hbs = exphbs.create({ defaultLayout: 'main', extname: '.hbs' })
+
+app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+hbs.handlebars.registerHelper('selectOption', function (category, listOption) {
+  return category === listOption ? new Handlebars.SafeString('<option selected>' + listOption + '</option>') : new Handlebars.SafeString('<option>' + listOption + '</option>')
+})
 
 
 const Record = require('./models/record')
@@ -43,6 +51,42 @@ app.post('/records', (req, res) => {
       res.redirect('./')
     })
     .catch(error => console.log(error))
+})
+
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  Category.find()
+    .lean()
+    .then(categories => {
+      Record.findById(id)
+        .lean()
+        .then(record => {
+          category = record.category
+          res.render('edit', { categories, record, category })
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+})
+
+app.put('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  Category.findOne({ name: { $regex: category, $options: 'i' } })
+    .lean()
+    .then(ctg => {
+      Record.findById(id)
+        .then(record => {
+          record.name = name
+          record.date = date
+          record.category = category
+          record.amount = amount
+          record.icon = ctg.icon
+          record.save()
+        })
+    }).then(() => {
+      res.redirect('/')
+    }).catch(error => console.log(error))
 })
 
 app.listen(port, () => {
