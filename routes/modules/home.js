7a39519f5cac75ece9ff367288e315f9d7cd8592
konnono <konnono@gmail.com>
2021-08-14 15:57:06 +0800

    [Feat] Add year and month filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,20 +4,68 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 router.get('/', (req, res) => {
-  const filter = req.query.filter ? { category: req.query.filter } : {}
-  Category.find()
+  const categoryFilter = req.query.categoryFilter ? { category: req.query.categoryFilter } : {}
+  const yearFilter = req.query.yearFilter
+  const monthFilter = req.query.monthFilter
+  // console.log(categoryFilter, yearFilter, monthFilter)
+
+  let startDate
+  let endDate
+  let dateFilter = {}
+
+  if (yearFilter && monthFilter) {
+    startDate = new Date(yearFilter, monthFilter - 1, 1)
+    const lastDay = new Date(yearFilter, monthFilter, 0).getDate()
+    endDate = new Date(yearFilter, monthFilter - 1, lastDay, 23, 59, 59)
+    // console.log(startDate.toLocaleString(), endDate.toLocaleString())
+    dateFilter = { date: { $gte: startDate, $lte: endDate } }
+  }
+
+  if (yearFilter && !monthFilter) {
+    startDate = new Date(yearFilter, 0, 1)
+    endDate = new Date(yearFilter, 11, 31, 23, 59, 59)
+    // console.log(startDate.toLocaleString(), endDate.toLocaleString())
+    dateFilter = { date: { $gte: startDate, $lte: endDate } }
+  }
+
+  const years = []
+  Record.find()
     .lean()
-    .then(categories => {
-      Record.find(filter)
+    .sort({ date: 'desc' })
+    .then(records => {
+      //產生年度的array作為年度篩選的下拉選項
+      const endYear = records[0].date.getFullYear()
+      const startYear = records[records.length - 1].date.getFullYear()
+
+      for (let i = startYear; i <= endYear; i++) {
+        years.push(i)
+      }
+    })
+    .then(() => {
+      // console.log('dateFilter:', dateFilter)
+      Category.find()
         .lean()
-        .sort({ date: 'desc' })
-        .then(records => {
-          res.render('index', { records, categories, filter: req.query.filter })
+        .then(categories => {
+          Record.find({ $and: [categoryFilter, dateFilter] })
+            .lean()
+            .sort({ date: 'desc' })
+            .then(records => {
+              if (!yearFilter && monthFilter) {
+                // console.log('monthFilter:', monthFilter)
+                // records.map(record => console.log(record.name, record.date.getMonth() + 1))
+                records = records.filter(record => (record.date.getMonth() + 1).toString() === monthFilter)
+
+                // console.log('records', records)
+              }
+              res.render('index', { records, categories, years, months, categoryFilter: req.query.categoryFilter, yearFilter: req.query.yearFilter, monthFilter: req.query.monthFilter })
+            })
+            .catch(error => console.log(error))
         })
         .catch(error => console.log(error))
     })
-    .catch(error => console.log(error))
 })
 
 module.exports = router
