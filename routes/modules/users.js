3864dca8007fb558933bc9bfb0e1e10a726659f1
konnono <konnono@gmail.com>
2021-08-15 21:58:49 +0800

    [Feat]: add bcrypt to handle password

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -51,7 +52,14 @@ router.post('/register', (req, res) => {
         return res.render('register', { name, email, password, confirmPassword })
       }
 
-      User.create({ name, email, password, confirmPassword })
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
     })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
