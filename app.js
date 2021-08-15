339c5d5df46b8d712d91c34c85ac534f690c35db
konnono <konnono@gmail.com>
2021-08-15 17:25:53 +0800

    [Feat] Add express-session

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('./node_modules/body-parser')
 const methodOverride = require('method-override')
-const Handlebars = require('handlebars')
+const session = require('express-session')
+
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -22,6 +23,12 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
 
 app.listen(port, () => {
