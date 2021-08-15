b8d24f8c6126a4395fea6a61447d5a467151d212
konnono <konnono@gmail.com>
2021-08-15 22:47:48 +0800

    [Feat] Modify seeder files

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI
-
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log(`mongodb error @ ${MONGODB_URI}`)
+  console.log('mongodb error!')
 })
 db.once('open', () => {
-  console.log(`mongodb connected @ ${MONGODB_URI}!`)
+  console.log('mongodb connected!')
 })
 
 module.exports = db
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,16 +1,48 @@
-const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Record = require('../record')
-const records = require('./records.json')
+const User = require('../user')
+const db = require('../../config/mongoose')
+
+const recordList = require('./records.json')
+const SEED_USERS = [
+  {
+    email: 'user1@example.com',
+    password: '12345678',
+    records: recordList.slice(0, 4)
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678',
+    records: recordList.slice(5, 9)
+  }
+]
 
 db.once('open', () => {
-  Promise.all(records)
-    .then(record => {
-      console.log(record)
-      return Record.create(record)
+  return Promise.all(SEED_USERS.map(async user => {
+    const records = user.records
+    await User.create({
+      email: user.email,
+      password: bcrypt.hashSync(user.password, bcrypt.genSaltSync(10))
     })
+      .then(user => {
+        return Promise.all(records.map(async record => {
+          await Record.create({
+            name: record.name,
+            date: record.date,
+            category: record.category,
+            amount: record.amount,
+            merchant: record.merchant,
+            userId: user._id
+          })
+        }))
+      })
+  }))
     .then(() => {
-      console.log('Record seeder completed!')
-      db.close()
+      console.log('done!')
+      process.exit()
     })
-    .catch(err => console.log(err))
 })
